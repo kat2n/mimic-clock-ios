3532308a63a823d38827fb30b932017d824e41f2
kat2n <katsumata.coffee.150yen@gmail.com>
2019-06-03 14:53:56 -0700

    created MimicClock

---
+++ MimicClock/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MimicClock
+//
+//  Created by Daisuke Katsumata on 2019-06-03.
+//  Copyright © 2019 kat8n. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
