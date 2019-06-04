a7be56662ac093b7ac8ab90b2cb7028b9baa0801
kat2n <katsumata.coffee.150yen@gmail.com>
2019-06-04 16:30:37 -0700

    styled UITabBar

--- MimicClock/Models/Color.swift
+++ MimicClock/Models/Color.swift
@@ -4,8 +4,9 @@ class Color {
 
   static let white = UIColor(red: 254 / 255, green: 255 / 255, blue: 255 / 255, alpha: 1)
   static let orange = UIColor(red: 239 / 255, green: 154 / 255, blue: 56 / 255, alpha: 1)
-  static let gray = UIColor(red: 56 / 255, green: 56 / 255, blue: 57 / 255, alpha: 1)
-  static let dark = UIColor(red: 23 / 255, green: 24 / 255, blue: 25 / 255, alpha: 1)
+  static let gray = UIColor(red: 127 / 255, green: 128 / 255, blue: 129 / 255, alpha: 1)
+  static let darkGray = UIColor(red: 56 / 255, green: 56 / 255, blue: 57 / 255, alpha: 1)
+  static let lightBlack = UIColor(red: 23 / 255, green: 24 / 255, blue: 25 / 255, alpha: 1)
   static let black = UIColor(red: 12 / 255, green: 13 / 255, blue: 14 / 255, alpha: 1)
 
 }
--- MimicClock/Views/TabBarController.swift
+++ MimicClock/Views/TabBarController.swift
@@ -7,11 +7,30 @@ class TabBarController: UITabBarController {
 
     UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: Color.white]
     UINavigationBar.appearance().tintColor = Color.orange
-    UINavigationBar.appearance().barTintColor = Color.dark
+    UINavigationBar.appearance().barTintColor = Color.lightBlack
     UINavigationBar.appearance().isTranslucent = false
     UINavigationBar.appearance().shadowImage = {
       UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
-      UIGraphicsGetCurrentContext()?.setFillColor(Color.gray.cgColor)
+      UIGraphicsGetCurrentContext()?.setFillColor(Color.darkGray.cgColor)
+      UIGraphicsGetCurrentContext()?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+      let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
+      UIGraphicsEndImageContext()
+      return image
+    }()
+
+    UITabBar.appearance().selectedImageTintColor = Color.orange
+    UITabBar.appearance().unselectedItemTintColor = Color.gray
+    UITabBar.appearance().backgroundImage = {
+      UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+      UIGraphicsGetCurrentContext()?.setFillColor(Color.lightBlack.cgColor)
+      UIGraphicsGetCurrentContext()?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+      let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
+      UIGraphicsEndImageContext()
+      return image
+    }()
+    UITabBar.appearance().shadowImage = {
+      UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+      UIGraphicsGetCurrentContext()?.setFillColor(Color.darkGray.cgColor)
       UIGraphicsGetCurrentContext()?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
       let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
       UIGraphicsEndImageContext()
