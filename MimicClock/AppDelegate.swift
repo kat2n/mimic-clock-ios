6652b5673c9cdb4b25827347ba19ce30cbffa9e4
kat2n <katsumata.coffee.150yen@gmail.com>
2019-06-04 16:33:13 -0700

    moved settings of appearance

--- MimicClock/AppDelegate.swift
+++ MimicClock/AppDelegate.swift
@@ -8,6 +8,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
 
+    UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: Color.white]
+    UINavigationBar.appearance().tintColor = Color.orange
+    UINavigationBar.appearance().barTintColor = Color.lightBlack
+    UINavigationBar.appearance().isTranslucent = false
+    UINavigationBar.appearance().shadowImage = {
+      UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+      UIGraphicsGetCurrentContext()?.setFillColor(Color.darkGray.cgColor)
+      UIGraphicsGetCurrentContext()?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+      let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
+      UIGraphicsEndImageContext()
+      return image
+    }()
+
+    UITabBar.appearance().selectedImageTintColor = Color.orange
+    UITabBar.appearance().unselectedItemTintColor = Color.gray
+    UITabBar.appearance().backgroundImage = {
+      UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+      UIGraphicsGetCurrentContext()?.setFillColor(Color.lightBlack.cgColor)
+      UIGraphicsGetCurrentContext()?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+      let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
+      UIGraphicsEndImageContext()
+      return image
+    }()
+    UITabBar.appearance().shadowImage = {
+      UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+      UIGraphicsGetCurrentContext()?.setFillColor(Color.darkGray.cgColor)
+      UIGraphicsGetCurrentContext()?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+      let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
+      UIGraphicsEndImageContext()
+      return image
+    }()
+
     window = UIWindow(frame: UIScreen.main.bounds)
     window?.rootViewController = TabBarController()
     window?.makeKeyAndVisible()
--- MimicClock/Views/TabBarController.swift
+++ MimicClock/Views/TabBarController.swift
@@ -5,38 +5,6 @@ class TabBarController: UITabBarController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: Color.white]
-    UINavigationBar.appearance().tintColor = Color.orange
-    UINavigationBar.appearance().barTintColor = Color.lightBlack
-    UINavigationBar.appearance().isTranslucent = false
-    UINavigationBar.appearance().shadowImage = {
-      UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
-      UIGraphicsGetCurrentContext()?.setFillColor(Color.darkGray.cgColor)
-      UIGraphicsGetCurrentContext()?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
-      let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
-      UIGraphicsEndImageContext()
-      return image
-    }()
-
-    UITabBar.appearance().selectedImageTintColor = Color.orange
-    UITabBar.appearance().unselectedItemTintColor = Color.gray
-    UITabBar.appearance().backgroundImage = {
-      UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
-      UIGraphicsGetCurrentContext()?.setFillColor(Color.lightBlack.cgColor)
-      UIGraphicsGetCurrentContext()?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
-      let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
-      UIGraphicsEndImageContext()
-      return image
-    }()
-    UITabBar.appearance().shadowImage = {
-      UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
-      UIGraphicsGetCurrentContext()?.setFillColor(Color.darkGray.cgColor)
-      UIGraphicsGetCurrentContext()?.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
-      let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
-      UIGraphicsEndImageContext()
-      return image
-    }()
-
     let worldClock = UINavigationController(rootViewController: WorldClockViewController())
     let alarm = UINavigationController(rootViewController: AlarmViewController())
     let bedtime = UINavigationController(rootViewController: BedtimeViewController())
